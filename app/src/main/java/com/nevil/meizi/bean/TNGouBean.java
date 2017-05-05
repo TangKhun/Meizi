da40cfecf917e77422c8fbf102b9913c607f02c7
TangKhun <TangKhun@163.com>
2017-05-05 18:22:56 +0800

    天狗Api Bean

---
+++ app/src/main/java/com/nevil/meizi/bean/TNGouBean.java
@@ -0,0 +1,58 @@
+package com.nevil.meizi.bean;
+
+import java.util.List;
+
+/**
+ * Created by Tangkun on 2017/5/5.
+ */
+
+public class TNGouBean {
+    String status;
+    String total;
+    List<TNGouDataBean> tngou;
+
+    public TNGouBean() {
+    }
+
+    public TNGouBean(String status, String total, List<TNGouDataBean> tngou) {
+        this.status = status;
+        this.total = total;
+        this.tngou = tngou;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public TNGouBean setStatus(String status) {
+        this.status = status;
+        return this;
+    }
+
+    public String getTotal() {
+        return total;
+    }
+
+    public TNGouBean setTotal(String total) {
+        this.total = total;
+        return this;
+    }
+
+    public List<TNGouDataBean> getTngou() {
+        return tngou;
+    }
+
+    public TNGouBean setTngou(List<TNGouDataBean> tngou) {
+        this.tngou = tngou;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "TNGouBean{" +
+                "status='" + status + '\'' +
+                ", total='" + total + '\'' +
+                ", tngou=" + tngou +
+                '}';
+    }
+}
