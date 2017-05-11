a24bd7afdc318f10f8a6170e255d3d2fca0b8a8a
TangKhun <TangKhun@163.com>
2017-05-11 18:14:51 +0800

    修改主页虚拟键样式，透明虚拟键

--- app/src/main/java/com/nevil/meizi/adapter/GankMeiziAdapter.java
+++ app/src/main/java/com/nevil/meizi/adapter/GankMeiziAdapter.java
@@ -1,8 +1,12 @@
 package com.nevil.meizi.adapter;
 
+import android.app.Activity;
+import android.app.ActivityOptions;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.support.annotation.Nullable;
+import android.widget.ImageView;
 
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.BaseViewHolder;
@@ -28,13 +32,21 @@ public class GankMeiziAdapter extends BaseQuickAdapter<GankDataBean, BaseViewHol
 
     @Override
     protected void convert(BaseViewHolder holder, GankDataBean gankDataBean) {
-        GlideImageManager.loadImage(mContext,gankDataBean.getUrl(),holder.getView(R.id.recycle_image));
-       // Glide.with(mContext).load(gankDataBean.getUrl()).placeholder(R.drawable.glide_empty).error(R.drawable.glide_err).into((ImageView) holder.getView(R.id.recycle_image));
+        GlideImageManager.loadImage(mContext, gankDataBean.getUrl(), holder.getView(R.id.recycle_image));
+        // Glide.with(mContext).load(gankDataBean.getUrl()).placeholder(R.drawable.glide_empty).error(R.drawable.glide_err).into((ImageView) holder.getView(R.id.recycle_image));
         holder.setOnClickListener(R.id.recycle_image, v -> {
             Intent intent = new Intent(mContext, PictureShowActivity.class);
             intent.putExtra("path", gankDataBean.getUrl());
-            mContext.startActivity(intent);
+            if (Build.VERSION.SDK_INT >= 21) {
+                ImageView imageView = holder.getView(R.id.recycle_image);
+                imageView.setTransitionName("image");
+                ActivityOptions options = ActivityOptions
+                        .makeSceneTransitionAnimation((Activity) mContext, imageView, "image");
+                mContext.startActivity(intent, options.toBundle());
+            }else {
 
+                mContext.startActivity(intent);
+            }
         });
     }
 
