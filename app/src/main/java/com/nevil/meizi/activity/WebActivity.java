c21039c350e4c06ab4b6c443ddf60552147b4db8
TangKhun <tangkhun@163.com>
2017-08-18 11:04:17 +0800

    web增加使用系统浏览器打开

--- app/src/main/java/com/nevil/meizi/activity/WebActivity.java
+++ app/src/main/java/com/nevil/meizi/activity/WebActivity.java
@@ -1,9 +1,13 @@
 package com.nevil.meizi.activity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.net.http.SslError;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.webkit.SslErrorHandler;
 import android.webkit.WebChromeClient;
@@ -14,6 +18,7 @@ import android.widget.ProgressBar;
 
 import com.nevil.meizi.R;
 import com.nevil.meizi.base.BaseActivity;
+import com.nevil.meizi.util.FileUtil;
 
 import butterknife.BindView;
 import butterknife.OnClick;
@@ -40,11 +45,10 @@ public class WebActivity extends BaseActivity {
 
         url = getIntent().getStringExtra("url");
         mToolbar.setTitle(getIntent().getStringExtra("TITLE"));
-      //  Log.e("MEIZI", "initView: " + url);
         setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        mToolbar.setSubtitle(url);
         mToolbar.setNavigationOnClickListener(v -> {
-           // Log.e("MEIZI", "initView: --------");
             WebActivity.this.finish();
         });
 
@@ -82,5 +86,24 @@ public class WebActivity extends BaseActivity {
     protected void onDestroy() {
         mWeb.destroy();
         super.onDestroy();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.web_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_browser) {
+            Intent intent = new Intent();
+            intent.setAction("android.intent.action.VIEW");
+            intent.setData(Uri.parse(url));
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
